bda69b3d9b1dc3faad1a124b8c45fce2eeb545b2
GrishaDesai <grishadesai78@gmail.com>
2026-02-22 16:51:04 +0530

    build error solved 3

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import type { FC } from 'react';
 import shoppingBag from '../assets/image/shopping_bag.png';
 
